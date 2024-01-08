436065b64b77dca88287de1d2060d0363b48462f
Pepijn de Vries <pepijn.devries@outlook.com>
2024-01-08 07:40:06 +0000

    version 0.2.0

--- R/copernicus_product_details.r
+++ R/copernicus_product_details.r
@@ -1,15 +1,15 @@
 #' Obtain details for a specific Copernicus marine product
 #'
-#' `r lifecycle::badge('stable')` Obtain details for a specific Copernicus marine product. This can be
+#' `r lifecycle::badge('deprecated')` Obtain details for a specific Copernicus marine product. This can be
 #' narrowed down to specific layers and/or variables within the product.
 #'
 #' @inheritParams copernicus_download_motu
-#' @return Returns a named `list` with properties of the requested product.
+#' @returns Returns a named `list` with properties of the requested product.
 #' @rdname copernicus_product_details
 #' @name copernicus_product_details
 #' @family product-functions
 #' @examples
-#' \donttest{
+#' \dontrun{
 #' copernicus_product_details("GLOBAL_ANALYSISFORECAST_PHY_001_024")
 #' 
 #' copernicus_product_details(
@@ -21,20 +21,21 @@
 #' @author Pepijn de Vries
 #' @export
 copernicus_product_details <- function(product, layer, variable) {
+  .Deprecated("cms_product_details")
   if (missing(layer) && !missing(variable)) stop("Variable specified without layer.")
   if (missing(product)) product <- ""
   result <- .try_online({
-    sprintf("https://data-be-prd.marine.copernicus.eu/api/dataset/%s?variant=detailed-v2", product) %>%
-      httr::GET()
-  }, "Copernicus") 
+    sprintf("https://data-be-prd.marine.copernicus.eu/api/dataset/%s?variant=detailed-v2", product) |>
+      httr2::request() |>
+      httr2::req_perform()
+  }, "Copernicus")
   if (is.null(result)) return(NULL)
   
   result <-
-    result %>%
-    httr::content("text") %>%
-    jsonlite::fromJSON()
+    result |>
+    httr2::resp_body_json()
   if (!missing(layer)) {
-    result <- result$layers[names(result$layers) %>% startsWith(paste0(layer, "/"))]
+    result <- result$layers[names(result$layers) |> startsWith(paste0(layer, "/"))]
   }
   if (!missing(variable)) {
     result <- result[[paste0(c(layer, variable), collapse = "/")]]
@@ -44,23 +45,24 @@ copernicus_product_details <- function(product, layer, variable) {
 
 #' Obtain available services for a specific Copernicus marine product
 #'
-#' `r lifecycle::badge('stable')` Obtain an overview of services provided by Copernicus
+#' `r lifecycle::badge('deprecated')` Obtain an overview of services provided by Copernicus
 #' for a specific marine product.
 #'
 #' @inheritParams copernicus_download_motu
-#' @return Returns a `tibble` with a list of available services for a
+#' @returns Returns a `tibble` with a list of available services for a
 #' Copernicus marine product
 #' @rdname copernicus_product_services
 #' @name copernicus_product_services
 #' @examples
-#' \donttest{
+#' \dontrun{
 #' copernicus_product_services("GLOBAL_ANALYSISFORECAST_PHY_001_024")
 #' }
 #' @author Pepijn de Vries
 #' @export
 copernicus_product_services <- function(product) {
+  .Deprecated("cms_product_details")
   services <- copernicus_product_details(product)
-  services <- services$services %>% purrr::map_dfr(dplyr::as_tibble) %>%
+  services <- services$services |> purrr::map_dfr(dplyr::as_tibble) |>
     dplyr::mutate(layer = {
       nms <- names(services$services)
       nms[nms != ""] })
--- R/wms.r
+++ R/wms.r
@@ -1,15 +1,15 @@
 #' Obtain a WMS entry for specific Copernicus marine products
 #'
-#' `r lifecycle::badge('experimental')` Web Map Services are not available for all
+#' `r lifecycle::badge('deprecated')` Web Map Services are not available for all
 #' products and layers. Use this function to obtain URLs of WMS services if any.
-#' @template wms_template
+#' @note WMS functions don't work on systems that don't support GDAL utils
 #' @inheritParams copernicus_download_motu
 #' @returns Returns a `tibble` with WMS URLs and descriptors for the specified product.
 #' @rdname copernicus_wms_details
 #' @name copernicus_wms_details
 #' @family wms-functions
 #' @examples
-#' \donttest{
+#' \dontrun{
 #' copernicus_wms_details(
 #'   product  = "GLOBAL_ANALYSISFORECAST_PHY_001_024",
 #'   layer    = "cmems_mod_glo_phy-thetao_anfc_0.083deg_P1D-m",
@@ -19,25 +19,26 @@
 #' @author Pepijn de Vries
 #' @export
 copernicus_wms_details <- function(product, layer, variable) {
+  .Deprecated("cms_get_wmts_details")
   product_details <- copernicus_product_details(product, layer, variable)
   if (is.null(product_details)) return(NULL)
 
   copwmsinfo <- sf::gdal_utils("info", paste0("WMS:", product_details$wmsUrl), quiet = TRUE)
   
-  desc <- copwmsinfo %>% stringr::str_match_all("SUBDATASET_(\\d)_DESC=(.*?)\n")
+  desc <- copwmsinfo |> stringr::str_match_all("SUBDATASET_(\\d)_DESC=(.*?)\n")
   if (length(desc) == 0) return(dplyr::tibble(desc = character(0), url = character(0)))
   desc <- desc[[1]][,3]
-  url  <- copwmsinfo %>% stringr::str_match_all("SUBDATASET_(\\d)_NAME=(.*?)\n")
+  url  <- copwmsinfo |> stringr::str_match_all("SUBDATASET_(\\d)_NAME=(.*?)\n")
   url  <- url[[1]][,3]
   return(dplyr::bind_cols(desc = desc, url = url))
 }
 
 #' Add Copernicus Marine WMS Tiles to a leaflet map
 #'
-#' `r lifecycle::badge('experimental')` Create an interactive map with
+#' `r lifecycle::badge('deprecated')` Create an interactive map with
 #' `leaflet::leaflet()` and add layers of Copernicus marine WMS data
 #' to it.
-#' @template wms_template
+#' @note WMS functions don't work on systems that don't support GDAL utils
 #' @param map A map widget object created from [`leaflet::leaflet()`]
 #' @inheritParams copernicus_download_motu
 #' @param options Passed on to [`leaflet::addWMSTiles()`].
@@ -47,11 +48,11 @@ copernicus_wms_details <- function(product, layer, variable) {
 #' @name addCopernicusWMSTiles
 #' @family wms-functions
 #' @examples
-#' \donttest{
+#' \dontrun{
 #' if (interactive()) {
-#'   leaflet::leaflet() %>%
-#'     leaflet::setView(lng = 3, lat = 54, zoom = 4) %>%
-#'     leaflet::addProviderTiles("Esri.WorldImagery") %>%
+#'   leaflet::leaflet() |>
+#'     leaflet::setView(lng = 3, lat = 54, zoom = 4) |>
+#'     leaflet::addProviderTiles("Esri.WorldImagery") |>
 #'     addCopernicusWMSTiles(
 #'       product  = "GLOBAL_ANALYSISFORECAST_PHY_001_024",
 #'       layer    = "cmems_mod_glo_phy-thetao_anfc_0.083deg_P1D-m",
@@ -63,6 +64,7 @@ copernicus_wms_details <- function(product, layer, variable) {
 addCopernicusWMSTiles <- function(map, product, layer, variable,
                                   options = leaflet::WMSTileOptions(format = "image/png", transparent = TRUE),
                                   ...) {
+  .Deprecated("addCmsWMSTTiles")
   detail <- copernicus_product_details(product, layer, variable)
   if (is.null(detail)) return(NULL)
   leaflet::addWMSTiles(
@@ -76,14 +78,15 @@ addCopernicusWMSTiles <- function(map, product, layer, variable,
 
 #' Extract and store WMS as a geo-referenced TIFF
 #'
-#' `r lifecycle::badge('experimental')` Extract and store imagery from a Copernicus WMS
-#'  as a geo-referenced TIFF.
+#' `r lifecycle::badge('deprecated')` This function interacts with deprecated Copernicus Marine
+#' Services. It will become [`.Defunct()`] in future versions. Extract and store imagery from a
+#' Copernicus WMS as a geo-referenced TIFF.
 #'
 #' A Web Map Service (WMS) cannot be plotted directly (base, ggplot2 and/or lattice).
 #' For that purpose you need to extract and download a specific region in a format
 #' that can be handled by plots. You can use this function to store a subset of a
 #' WMS map as a geo-referenced TIFF file.
-#' @template wms_template
+#' @note WMS functions don't work on systems that don't support GDAL utils
 #' @inheritParams copernicus_download_motu
 #' @param destination File name for the geo-referenced TIFF.
 #' @param width Width in pixels of the TIFF image.
@@ -93,7 +96,7 @@ addCopernicusWMSTiles <- function(map, product, layer, variable,
 #' @name copernicus_wms2geotiff
 #' @family wms-functions
 #' @examples
-#' \donttest{
+#' \dontrun{
 #' destination <- tempfile("wms", fileext = ".tiff")
 #' copernicus_wms2geotiff(
 #'   product     = "GLOBAL_ANALYSISFORECAST_PHY_001_024",
@@ -108,14 +111,15 @@ addCopernicusWMSTiles <- function(map, product, layer, variable,
 #' @author Pepijn de Vries
 #' @export
 copernicus_wms2geotiff <- function(product, layer, variable, region, destination, width, height) {
+  .Deprecated(msg = "This function interacts with deprecated Copernicus Marine Services, it will be discontinued.")
   wms_details     <- copernicus_wms_details(product, layer, variable)
   product_details <- copernicus_product_details(product, layer, variable)
   if (is.null(wms_details) || is.null(product_details)) return(NULL)
   desc            <- NULL # <- silences R checks with respect to global bindings...
   url             <-
-    wms_details %>%
-    dplyr::filter(desc == variable | dplyr::n() == 1) %>%
-    dplyr::pull("url") %>%
+    wms_details |>
+    dplyr::filter(desc == variable | dplyr::n() == 1) |>
+    dplyr::pull("url") |>
     stringr::str_replace("BBOX=(.*?)$", paste0("BBOX=", paste0(region, collapse = ',')))
   sf::gdal_utils(
     "translate",
