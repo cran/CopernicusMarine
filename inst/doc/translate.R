2585a07bcf771ae2ea1392befd5bcfd9d4242340
Pepijn de Vries <pepijn.devries@outlook.com>
2025-11-11 22:30:10 +0000

    version 0.3.6

--- inst/doc/translate.R
+++ inst/doc/translate.R
@@ -26,6 +26,8 @@ translated <- cms_translate(cli_code)
 summary(translated)
 
 ## ----download-translation, message=FALSE, fig.width=4, fig.height=4, fig.alt="Data downloaded using translated query code"----
-result <- do.call(cms_download_subset, translated)
-plot(result, col = hcl.colors(100), axes = TRUE)
+if (requireNamespace("blosc")) {
+  result <- do.call(cms_download_subset, translated)
+  plot(result, col = hcl.colors(100), axes = TRUE)
+}
 
